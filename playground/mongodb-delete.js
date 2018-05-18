8c26b7efe0e13f92e4e5930a588847629d7e1f39
gms08 <gms@thesousas.com>
2018-05-18 09:44:02 -0500

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {
+    MongoClient,
+    ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // //deleteOne 
+    // db.collection('Todos').deleteOne({
+    //     text: 'Eat lunch'
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // //findOneAndDelete
+    // db.collection('Todos').findOneAndDelete({
+    //     completed: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    //removeDuplicates
+    db.collection('Users').deleteMany({
+        name: 'Andrew'
+    });
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("5a86978929ed740ca87e5c31")
+    }).then((results) => {
+        console.log(JSON.stringify(results, undefined, 2));
+    });
+
+    //client.close();
+});
